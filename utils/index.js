e1a7e5fc9d220779f858ac4183977ec79d03780a
duanyuwen <duanyuwen@meituan.com>
2018-09-22 18:47:48 +0800

    fix gitinit

--- utils/index.js
+++ utils/index.js
@@ -21,22 +21,6 @@ exports.sortDependencies = function sortDependencies(data) {
 }
 
 /**
- * Runs `git init` in the project directory
- * @param {string} cwd Path of the created project directory
- * @param {object} data Data from questionnaire
- */
-exports.gitInit = function gitInit(
-  cwd,
-  color
-) {
-  console.log(`\n\n# ${color("git init ...")}`);
-  console.log("# ========================\n");
-  return runCommand('git', ["init"], {
-    cwd
-  });
-};
-
-/**
  * Runs `npm install` in the project directory
  * @param {string} cwd Path of the created project directory
  * @param {object} data Data from questionnaire
@@ -161,3 +145,18 @@ function sortObject(object) {
     })
   return sortedObject
 }
+/**
+ * Runs `git init` in the project directory
+ * @param {string} cwd Path of the created project directory
+ * @param {object} data Data from questionnaire
+ */
+function gitInit(
+  cwd,
+  color
+) {
+  console.log(`\n\n# ${color("git init ...")}`);
+  console.log("# ========================\n");
+  return runCommand('git', ["init"], {
+    cwd
+  });
+};
